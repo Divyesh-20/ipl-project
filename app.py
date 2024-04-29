d5db68665bf67fdaff6352392f8c4990f9d018cb
mr_robot <49763984+ajstyle007@users.noreply.github.com>
2024-04-29 14:20:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ df2 = pd.read_csv("IPL_Matches_2008_2022.csv")
 #st.sidebar.title("This is IPL Analysis Project.")
 #st.sidebar.title("   IPL Data Analysis")
 
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
+hide_github_icon = “”"
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 st.sidebar.markdown(
