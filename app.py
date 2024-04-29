5fcdd7b3374d800f3eb188c47626228aaf7d0ba8
mr_robot <49763984+ajstyle007@users.noreply.github.com>
2024-04-29 14:05:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ hide_st_style = """
             <style>
             
             #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
+            
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
